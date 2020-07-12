1567d4d6e7b1d3d5c7a67fe63b757b0aa9de6273
Pudding <s.kokuryo@gmail.com>
2020-07-12 11:05:53 +0900

    Rewrite comments of others to follow Google style

--- uroboros/utils.py
+++ uroboros/utils.py
@@ -1,16 +1,22 @@
 def get_args_command_name(layer: int):
+    """Return the specified layer's command name"""
     return "__layer{layer}_command".format(layer=layer)
 
 
 def get_args_validator_name(layer: int):
+    """Return the specified layer's validator name"""
     return "__layer{layer}_validator".format(layer=layer)
 
 
 def get_args_section_name(layer: int):
+    """Return the specified layer's parser name"""
     return "__layer{layer}_parser".format(layer=layer)
 
 
 def call_one_by_one(objs, method_name: str, args, **kwargs):
+    """
+    Call specified method of given objects with given args in order.
+    """
     for obj in objs:
         assert hasattr(obj, method_name), \
             "'{cmd}' has no method '{method}".format(
